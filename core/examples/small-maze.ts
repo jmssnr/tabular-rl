21de64ee9e661adf7f1c7ec8358bc4143ba1386e
jmssnr <maussner.johannes@gmail.com>
2026-07-17 09:04:11 +0200

    updates examples

--- core/examples/small-maze.ts
+++ core/examples/small-maze.ts
@@ -1,10 +1,12 @@
-import { MazeWorld } from "@/core/environments/maze-world";
-import { KeyValueStore } from "@/core/utils/key-value-store";
+import { Grid } from "@/core/dynamics/grid";
+import { createMazeWorld } from "@/core/dynamics/worlds/maze";
 
-export const SMALLMAZE = new MazeWorld(
-  [5, 5],
-  new KeyValueStore([
-    [[2, 2], { kind: "obstacle" }],
-    [[4, 4], { kind: "terminal" }],
-  ]),
+export const SMALL_MAZE_WORLD = createMazeWorld(
+  new Grid(
+    [5, 5],
+    [
+      [[2, 2], { kind: "obstacle" }],
+      [[4, 4], { kind: "terminal" }],
+    ],
+  ),
 );
