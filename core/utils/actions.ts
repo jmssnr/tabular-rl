70f6da92c9789c5493be911608cf1ba681753530
jmssnr <maussner.johannes@gmail.com>
2026-07-17 08:25:59 +0200

    removes old version

--- core/utils/actions.ts
+++
@@ -1,8 +0,0 @@
-import { Action } from "@/core/types";
-
-export const ACTIONFOUR: Array<Action> = [
-  [0, 1],
-  [1, 0],
-  [0, -1],
-  [-1, 0],
-];
--- core/utils/add-defaults.ts
+++
@@ -1,6 +0,0 @@
-export function addDefaults<Config>(
-  config: Partial<Config>,
-  defaults: Config,
-): Config {
-  return { ...defaults, config };
-}
